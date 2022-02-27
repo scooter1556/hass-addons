2c263945144eb3fd2c6d43bc118432e98f248cb3
Scott Ware <scott.r.ware@intel.com>
2022-02-27 21:46:17 +0000

    glow2mqtt: Add initial version of plugin
    Signed-off-by: Scott Ware <scott.r.ware@intel.com>

---
+++ glow2mqtt/DOCS.md
@@ -0,0 +1,7 @@
+# Home Assistant Add-on: Hildebrand Glow To MQTT
+
+## Usage
+
+You will need to have an active Glow account (usable through the Bright app), a Consumer Access Device (CAD), and MQTT access enabled for your account before using this addon. If you haven't been given Glowmarkt MQTT connection details by Hildebrand, you'll need to contact them and request MQTT access be enabled for your account.
+
+## Configuration
---
+++ glow2mqtt/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bashio
+
+if bashio::services.available mqtt; then
+  echo "MQTT found in this Home Assistance instance."
+  MQTT_HOST=$(bashio::services mqtt "host")
+  MQTT_PORT=$(bashio::services mqtt "port")
+  export MQTT_USERNAME=$(bashio::services mqtt "username")
+  export MQTT_PASSWORD=$(bashio::services mqtt "password")
+else
+  echo "Using external MQTT broker."
+  MQTT_HOST=$(bashio::config "mqtt_host")
+  MQTT_PORT=$(bashio::config "mqtt_port")
+  export MQTT_USERNAME=$(bashio::config "mqtt_user")
+  export MQTT_PASSWORD=$(bashio::config "mqtt_password")
+fi
+
+GLOW_DEVICE=$(bashio::config "glow_device_id")
+GLOW_USERNAME=$(bashio::config "glow_username")
+GLOW_PASSWORD=$(bashio::config "glow_password")
+
+OTHER_ARGS=""
+
+if bashio::config.true "discovery"; then
+  OTHER_ARGS="${OTHER_ARGS} --homeassistant"
+fi
+
+echo "Starting glow2mqtt.py..."
+python3 -u /glow2mqtt.py --glow_device $GLOW_DEVICE --glow_username $GLOW_USERNAME --glow_password $GLOW_PASSWORD --mqtt_address $MQTT_HOST --mqtt_port $MQTT_PORT  --mqtt_username $MQTT_USERNAME --mqtt_password $MQTT_PASSWORD $OTHER_ARGS
